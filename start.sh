ca2059fb5c123442b11a4fd1fb128b4e170d6a4a
raidenii <raidenii@users.noreply.github.com>
2023-12-16 22:25:39 -0500

    fix dup

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 PUID=${PUID:-911}
 PGID=${PGID:-911}
 
+[ -z "$(getent passwd v2ray)" ] || deluser v2ray
 addgroup -g ${PGID} v2ray
 adduser -u ${PUID} -G v2ray -s /sbin/nologin -h /etc/v2ray -D v2ray
 chown -R v2ray:v2ray /etc/v2ray
